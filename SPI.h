185e6c3b18ccb35c4b2076edb82d91c10123e30e
Nazad66 <111635512+Nazad66@users.noreply.github.com>
2023-12-24 15:16:58 +0200

    SPI.h

--- SPI.h
+++ SPI.h
@@ -1,19 +1,22 @@
+/* SPI Registers addresses */     
 #define SPI_CR1 (* ((unsigned volatile long*)0x40013000))
 #define SPI_CR2 (* ((unsigned volatile long*)0x40013004))
 #define SPI_SR  (* ((unsigned volatile long*)0x40013008))
 #define SPI_DR  (* ((unsigned volatile long*)0x4001300C))
 
- /* Data frame selection */
+                                            /* MACROS */
+
+ //Data frame selection 
 #define bitMode8      0
 #define bitMode16     1
 
-/* Clock Phase and Polarity modes*/
+//Clock Phase and Polarity modes
 #define SPI_MODE0     0
 #define SPI_MODE1     1
 #define SPI_MODE2     2
 #define SPI_MODE3     3
 
- /* Baud Rate selection */
+//Baud Rate selection
 #define fPCLK2        0
 #define fPCLK4        1
 #define fPCLK8        2
@@ -23,10 +26,12 @@
 #define fPCLK128      6
 #define fPCLK256      7
 
- /* Transfer order */
+//Transfer order 
 #define LSBFIRST      1
 #define MSBFIRST      0
 
+
+                                   /* Functions Prototypes */
 
 void begin                      (void);
 void setDataMode                (int mode, int clockMode);
@@ -36,3 +41,4 @@ unsigned char transfer          (unsigned char TxData);
 void end                        (void);
 void beginTransaction           (int byteOrder,int dataMode,int baudRate,int clockMode);
 void endTransaction             (void);
+
