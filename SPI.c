c4030a7d1135f2aab9de33da9660dcd4984eb3ed
Nazad66 <111635512+Nazad66@users.noreply.github.com>
2023-12-24 15:16:28 +0200

    SPI.c

--- SPI.c
+++ SPI.c
@@ -1,29 +1,31 @@
 /* MCAL */
 #include "SPI.h"
-
+ 
+ unsigned char RxData ;
+ 
 void begin ()
 {
      //GPIO pins configuration
-      RCC_AHB1ENR |= (1<<0);                 //IO port A clock enable
+       RCC_AHB1ENR |= (1<<0);                 //IO port A clock enable
      // PA4(SPI1_NSS), PA5(SPI1_SCK), PA6(SPI1_MISO), PA7(SPI1_MOSI)
-      GPIOA_MODER |= (2<<8) | (2<<10) | (2<<12) | (2<<14)  ;          //(PA4,PA5,PA6,PA7) alternate mode 
-      GPIOA_AFRL  |= (5<<16) | (5<<20) | (5<<24) | (5<<28) ;         // SPI1 alternate functions
+       GPIOA_MODER |= (2<<8) | (2<<10) | (2<<12) | (2<<14)  ;          //(PA4,PA5,PA6,PA7) alternate mode 
+       GPIOA_AFRL  |= (5<<16) | (5<<20) | (5<<24) | (5<<28) ;         // SPI1 alternate functions
       
-     //SPI1 clock enable
-      RCC_APB2ENR |= (1<<12);
+      //SPI1 clock enable
+       RCC_APB2ENR |= (1<<12);
+      
+      //Software slave management enable
+       SPI_CR1 |=  (1<<9) ;           //SSM=1
+       SPI_CR1 |=  (1<<8) ;          // SSI=1 (to operate in master mode)
       
      //Full duplex mode
-      SPI_CR1 &= ~( (1<<10) | (1<<15) ) ;      //RxONLY=0 , BIDIMODE=0
+       SPI_CR1 &= ~( (1<<10) | (1<<15) ) ;      //RxONLY=0 , BIDIMODE=0
       
      //Master configuration
-      SPI_CR1 |= (1<<2);
-      
-     //Software slave management enable
-      SPI_CR1 |=  (1<<9) ;           //SSM=1
-      SPI_CR1 |=  (1<<8) ;           // SSI=1
+       SPI_CR1 |= (1<<2);
       
      //SPI enable
-      SPI_CR1 |= (1<<6);
+       SPI_CR1 |= (1<<6);
 
 }
 
@@ -137,26 +139,26 @@ void setBitOrder (int byteOrder)
 unsigned char transfer (unsigned char TxData)
 {
 
-         unsigned char RxData = 0;
+             unsigned char RxData = 0;
          
      //Wait for TXE=1 to indicate that transmit buffer is empty
         while( !((SPI_SR) & (1<<1)) );
 
       //Write data and dummy byte to data register 
-          SPI_DR = TxData << 8;
+          SPI_DR = TxData;
           
      /* Wait for  BSY=0  SPI is not busy  *transmission of the last data is complete*
                   RXNE=1 receive buffer is not empty  */
         while( !((SPI_SR) & (1<<0))  ||  SPI_SR & (1<<7) );
 
-          RxData = SPI_DR;
-          return RxData;
+          RxData = SPI_DR ;
+          return RxData ;
 }
 
 void end ()
 {
-      // Wait until (RXNE=1 , TXE=1 ,BSY=0)
-      while( !((SPI_SR) & (1<<0)) || !((SPI_SR) & (1<<1)) || SPI_SR &(1<<7) ) ;
+       // Wait until  BSY=0
+      while( SPI_SR &(1<<7) );
 
        //SPI disable
       SPI_CR1  &= ~(1<<6);
@@ -267,5 +269,10 @@ void beginTransaction (int byteOrder, int dataMode, int baudRate, int clockMode)
 
 void endTransaction ()
 {
+        // Wait until (RXNE=1 , TXE=1 ,BSY=0)
+       while( !((SPI_SR) & (1<<0)) || !((SPI_SR) & (1<<1)) || SPI_SR &(1<<7) ) ;
 
+       //SPI disable
+      SPI_CR1  &= ~(1<<6);
 }
+
